0d6111f7f4f36ed5d5c03501bd57bd36964d6536
Brendan <BP7714.bp@gmail.com>
2023-12-04 02:00:09 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 import {decodeVideo} from './modules/decodeVideo.js';
-import {decodeFrame} from './modules/decodeFrame.js';
-import fs from 'fs';
-const log = console.log.bind(console);
 
 // main entry point
 const main = async function() {
